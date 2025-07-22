331f4a046e1bb9da35a5df7ec36fd8f451effb89
Emmanuel Ajisope <emmanuel.ajisope@gmail.com>
2025-07-22 20:52:33 +0100

    Totaling major changes together, to make the newsletter suscription functionality.

---
+++ screens/SubscribeScreen.js
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import {Alert, TextInput, View, Pressable, StyleSheet, Image, Text, KeyboardAvoidingView } from 'react-native';
+import ButtonComponent from "../components/ButtonComponent";
+import {validateEmail} from "../utils";
+
+const SubscribeScreen = () => {
+
+  const [email, setEmail] = useState('');
+
+  const isEmailValid = validateEmail(email);
+
+  return (
+  // <KeyboardAvoidingView>
+    <View style={styles.container}>
+        <Image
+          source={require('../assets/little-lemon-logo-grey.png')}
+          style={styles.logo}
+        />
+
+        <Text style={styles.catchPhrase}>Subscribe to Our Newsletter for our latest delocious recipe</Text>
+        <TextInput style={{borderWidth: 1, width: "90%", padding: 15, height: 50}}  
+          onChangeText={setEmail} keyboardType="email-address" textContentType="emailAddress" 
+          placeholder="Type your email"/>
+
+        <ButtonComponent
+          onPress={() => {
+            Alert.alert("Successfully suscribed! We'll get in touch!");
+          }}
+          disabled={!isEmailValid}
+        >
+          Subscribe
+        </ButtonComponent>
+      </View>
+      )
+    {/* </KeyboardAvoidingView> */}
+    
+};
+
+export default SubscribeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "red",
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    marginTop: 100,
+  },
+  catchPhrase: {
+    fontSize: 20,
+    fontWeight: 400,
+    padding: 30,
+    textAlign: 'center',
+  },
+})
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { View, Image, Text, StyleSheet, Pressable } from 'react-native';
+// import SubscribeScreen from './SubscribeScreen';
+import ButtonComponent from "../components/ButtonComponent";
+
+const WelcomeScreen = ({ navigation }) => {
+  // Add welcome screen code here.
+  return <View style={styles.container}>
+    <Image 
+      source={require('../assets/little-lemon-logo.png')}
+      style={styles.logo} 
+    />
+    <Text style={styles.catchPhrase}>Little Lemon, your local Mediterranean  Bistro</Text>
+
+    <ButtonComponent onPress={()=>{navigation.navigate('SubscribeScreen')}}>
+      Navigation
+    </ButtonComponent>
+  </View>;
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: "red",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  catchPhrase: {
+    fontSize: 20,
+    fontWeight: 600,
+    padding: 30,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'green',
+    width: '90%',
+    borderRadius: 5,
+    margin: 10,
+    padding: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: 'center',
+  }
+})
